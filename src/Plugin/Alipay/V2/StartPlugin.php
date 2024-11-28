cd655a6b7a4c63f6089f4e00553d731d7ef6df57
liran <839669881@qq.com>
2024-11-28 20:28:23 +0800

    update

--- src/Plugin/Alipay/V2/StartPlugin.php
+++ src/Plugin/Alipay/V2/StartPlugin.php
@@ -197,14 +197,6 @@ class StartPlugin implements PluginInterface
         $dec = '0';
         $len = strlen($hex);
 
-        for ($i = 1; $i <= $len; ++$i) {
-            $dec = bcadd(
-                $dec,
-                bcmul(strval(hexdec($hex[$i - 1])), bcpow('16', strval($len - $i), 0), 0),
-                0
-            );
-        }
-
         return $dec;
     }
 }
